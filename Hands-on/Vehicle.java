87ffe0711a0d27c54dd0e574bc3780eb4f9eb0c6
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-07-05 20:02:15 +0530

    Vehicle.java

--- Hands-on/Vehicle.java
+++ Hands-on/Vehicle.java
@@ -1,15 +1,17 @@
 package oops;
 
-class Vehicle {
-   public String brand;
-   public String year;
+class Vehicle 
+{
+	//fields and methods
+   	public String brand;
+  	public String year;
 	public void displayinfo()
 	{
 		System.out.println("brand and year of the vehicle ");
 		
 	}
 }
-
+//inherits from class vehicle
  class Car extends Vehicle
  {
 	 public String model;
@@ -20,7 +22,7 @@ class Vehicle {
  }
  class Sportscar extends Car 
  {
-	 int topspeed;
+	 String topspeed;
 	 public void displayinfo()
 		{
 			System.out.println("brand,year,model and topspeed of the sportscar ");
@@ -32,15 +34,18 @@ class Vehicle {
 	
 		public static void main(String[] args)
 	{
-			Sportscar s=new Sportscar();
-			s.brand="ferrarri";
-			s.model="s1";
-			s.year="2023";
-			s.topspeed=999;
-			System.out.println("brand name is " +  s.brand);
-			System.out.println("model is " +  s.model);
-			System.out.println("year is " +  s.year);
-			System.out.println("topspeed is"+ s.topspeed);
-			}
+		//creating object of subclass
+	 	Sportscar s=new Sportscar();
+		
+		//access fields and methods
+		s.brand="Ferrari";
+		System.out.println("brand name is: " + s.brand);
+		s.year=2023;
+		System.out.println("year is: " +s.year);
+		s.model="488 GTB";
+		System.out.println("model name is: " + s.model);
+		s.topspeeed="205 mph";
+		System.out.println("topspeed is: " + s.topspeeed);
 	}
+ }
 	 
