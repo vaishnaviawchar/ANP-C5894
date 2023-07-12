936dd9210a02d1ec7adca2764bd6d9affaf5f705
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-07-12 14:00:40 +0530

    Add files via upload

---
+++ Practicals/Forloop.java
@@ -0,0 +1,13 @@
+package loops;
+//simple for loop program
+public class Forloop 
+{
+	public static void main(String[] args)
+	{
+		for(int i=9;i>0;i--)
+		{
+			System.out.println("Values of i is :"+ i);
+		}
+
+	}
+}
