9bbb27ba38d1e25fa6cab248d090bf2e76098068
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-07-14 14:35:26 +0530

    Add files via upload

---
+++ Practicals/DoWhileLoop.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) 
+	{
+		//initializing i to 1
+		int i = 1;
+		do
+		{
+			//print hello world
+			System.out.println("Hello World");
+			i++;
+		}
+		//test expression
+		while(i<6);
+	}
+
+}
