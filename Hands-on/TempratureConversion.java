633d5bfe0bc5f74a139e6aa210e040b3cdc4d83d
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-07-17 20:19:27 +0530

    TempratureConversion.java

--- Hands-on/TempratureConversion.java
+++ Hands-on/TempratureConversion.java
@@ -1,12 +1,21 @@
 package loops;
+/*Prompt the user to enter a temperature in Celsius.
+Provide a menu to choose the conversion type: Fahrenheit, Kelvin, or Rankine.
+Use a switch case statement to perform the selected conversion and display the result.
+Handle invalid conversion choices appropriately.*/
+
+//importing scanner class
 import java.util.Scanner;
-public class TempratureConversion 
+public class TemperatureConversion 
 {
 	public static void main(String[] args) 
 	{
+		//input from user
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the temprature in celcius : ");
-		int temprature=sc.nextInt();
+		
+		//input for temperature
+		int temperature=sc.nextInt();
 		
 		System.out.println("Available conversion type: ");
 		System.out.println("1.Fahrenheit");
@@ -14,26 +23,34 @@ public class TempratureConversion
 		System.out.println("3.Rankine");
 		System.out.println("Enter your choice : ");
 		
-		int choice=sc.nextInt();
+		//input variable for conversion
+		int conversion=sc.nextInt();
+		
+		//declaring variable
 		double result;
-        String conversionType;
+                String conversionType;
 
-        switch (choice) {
-            case 1:
-                result = (temprature * 9 / 5) + 32;
-                conversionType = "Fahrenheit";
-                break;
-            case 2:
-            	result = temprature + 273.15;
-                conversionType = "Kelvin";
-                break;
-            case 3:
-            	result = (temprature + 273.15) * 9 / 5;
-                conversionType = "Rankine";
-                break;
-            default:
-                System.out.println("Handle invalid conversion choices appropriately.");
-                return;
+                //switch case
+		switch (conversion) 
+		{
+                    case 1:
+                    result = (temperature * 9 / 5) + 32;
+                    conversionType = "Fahrenheit";
+                    break;
+            
+		    case 2:
+            	    result = temperature + 273.15;
+                    conversionType = "Kelvin";
+                    break;
+            
+		    case 3:
+            	    result = (temperature + 273.15) * 9 / 5;
+                    conversionType = "Rankine";
+                    break;
+            
+		    default:
+                    System.out.println("Handle invalid conversion choices appropriately.");
+                    return;
         }
 
         System.out.println("Converted temperature in " + conversionType + ": " + result);
