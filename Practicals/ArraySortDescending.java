c9d9549fd1a7f0038238d45bda7903ce1ba909c4
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-07-28 13:46:41 +0530

    Add files via upload

---
+++ Practicals/ArraySortDescending.java
@@ -0,0 +1,33 @@
+package array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySortDescending 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array: ");
+		int size = sc.nextInt();
+		
+		// Get the elements of the array from the user
+        // Using Integer wrapper class for sorting
+		Integer[] arr = new Integer[size];
+		System.out.println("Enter" + size + "elements: ");
+		for(int i=0; i<size; i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		// Sort the array in descending order using custom comparator
+		Arrays.sort(arr,(a,b)-> b.compareTo(a));
+		// Print the sorted array
+		System.out.print("Array in descending order: ");
+        for (int i = 0; i < size; i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+	}
+
+}
