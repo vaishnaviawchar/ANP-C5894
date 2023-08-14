3bd04d82eb22032fc8dc98c3f7cd170f6986a46a
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-08-14 14:02:22 +0530

    Add files via upload

---
+++ Hands-on/AgeChecker.java
@@ -0,0 +1,51 @@
+package exceptionhandling;
+/*Develop a program that takes a user's age as input and determines if they are eligible to vote (age >= 18).
+ *If the user enters non-numeric text or a negative number,
+ *catch the NumberFormatException and IllegalArgumentException respectively,displaying appropriate error message*/
+import java.util.Scanner;
+public class AgeChecker 
+{
+	public static void main(String[] args) 
+	{
+		//Scanner class to take input from user
+		Scanner sc = new Scanner(System.in);
+		
+		try
+		{ 
+			//Prompts user to enter a age
+			System.out.println("Enter your age: ");
+			
+			//This line can throw NumberFormatException
+			int age = Integer.parseInt(sc.nextLine());
+			
+			//if condition to check age is less than zero
+			if(age<0)
+			{
+				System.out.println("Error: Age cannot be negative");
+			}
+			//if condition to check age is less than equal to 18
+		    if(age>=18)
+			{
+				System.out.println("Yor are eligible for vote! ");
+			}
+			else
+			{
+				System.out.println("You are not eligible for vote");
+			}
+			
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Error: Please enter a valid numeric age.");
+		}
+		catch (IllegalArgumentException e) 
+		{
+            System.out.println("Error: " + e.getMessage());
+		}
+		
+		//closing the scanner class object
+		sc.close();
+
+	}
+
+}
---
+++ Hands-on/ArithemeticOperationCalculator.java
@@ -0,0 +1,87 @@
+package exceptionhandling;
+/*Write a program that takes two numbers and an operator (+, -, *, /) from the user.
+ * Perform the corresponding arithmetic operation and display the result.
+ * Handle exceptions for invalid operators and division by zero.*/
+import java.util.Scanner;
+public class ArithemeticOperationCalculator 
+{
+	public static void main(String[] args) 
+	{
+		//Scanner class to take input from user
+		Scanner sc = new Scanner(System.in);
+		
+		//prompts user to enter first number
+		System.out.println("Enter a first number : ");
+		//integer input for number1
+		int number1 = sc.nextInt();
+		
+		//prompts user to enter second number
+		System.out.println("Enter a second number: ");
+		//integer input for number2
+		int number2 = sc.nextInt();
+		
+		System.out.println("Available Operators: ");
+		System.out.println("1. + ");
+		System.out.println("2. - ");
+		System.out.println("3. * ");
+		System.out.println("4. / ");
+		System.out.println("Choose the Operator: ");
+	    char operator=sc.next().charAt(0);
+	    //Declaring variable result
+		int result;
+		
+		try
+		{
+			//if condition to check operator is +
+			if(operator == '+')
+			{
+				result= number1 + number2;
+				System.out.println("The Addition is: "+result);
+			   
+			}
+			
+			//else if condition to check operator is -
+			else if(operator == '-')
+			{
+				result = number1 - number2;
+				System.out.println("The Substraction is: " +result);
+			  
+			}
+			
+			//else if condition to check operator is *
+			else if(operator == '*')
+			{
+				 result= number1 * number2;
+				 System.out.println("The Multiplication is: " +result);
+		      
+			}
+			//else if condition to check operator is /
+			else if(operator == '/')
+			{
+				if (number2 != 0) 
+				{
+					result = number1 / number2;
+					System.out.println("The Divivsion is: " +result);
+				}
+	            else 
+	            {
+	            	System.out.println("Error: Division by zero is not allowed");
+	            }
+			}
+			
+			else
+			{
+				System.out.println("Error: Invalid operator!!");
+			}
+			
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Error: " +e.getMessage());
+		}
+
+		//closing the scanner class object
+		sc.close();
+	}
+
+}
