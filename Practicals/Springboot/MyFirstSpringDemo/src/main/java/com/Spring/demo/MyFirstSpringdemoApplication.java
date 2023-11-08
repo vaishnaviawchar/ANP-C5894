1f4895652c83f6e0ca83ea9da5003cb1b9c543fc
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-11-08 14:13:56 +0530

    Add files via upload

---
+++ Practicals/Springboot/MyFirstSpringDemo/src/main/java/com/Spring/demo/MyFirstSpringdemoApplication.java
@@ -0,0 +1,15 @@
+package com.Spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringdemoApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(MyFirstSpringdemoApplication.class, args);
+	}
+
+}
