da87117a055b47f1c1789c14808e6d69a7031399
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-11-21 22:01:31 +0530

    Add files via upload

---
+++ Practicals/SpringBootWithAngular/EmployeeBackendProject/src/main/java/com/SpringEmployee/demo/EmployeeBackendProjApplication.java
@@ -0,0 +1,13 @@
+package com.SpringEmployee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBackendProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBackendProjApplication.class, args);
+	}
+
+}
