915e5eca8671e4cfe73286ce093cd7a32654ac06
vaishnaviawchar <88968946+vaishnaviawchar@users.noreply.github.com>
2023-11-21 19:37:37 +0530

    Add files via upload

---
+++ Practicals/Springboot/src/main/java/com/Spring/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.Spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController 
+{
+	@GetMapping("/hello")
+	public String hrlloworld()
+	{
+		return"welcome";
+	}
+
+}
---
+++ Practicals/Springboot/src/main/java/com/SpringEmployee/demo/Model/Employee.java
@@ -0,0 +1,24 @@
+package com.SpringEmployee.demo.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee 
+{ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private String emailId;
+
+}
